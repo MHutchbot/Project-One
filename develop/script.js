c90a48ca6f11d4b69f9b88447bfbe7324c54ab46
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2020-10-29 19:36:28 -0600

    working on unSplash API

--- develop/script.js
+++ develop/script.js
@@ -1,1 +1,22 @@
-alert("Is this working?")
+var unsplashAPI = "https://api.unsplash.com/photos/random/?client_id=nTDcb20SlPlnOUb5B3Z5i9q1D8woTYppcHS-nLzA2Ho"
+
+fetch(unsplashAPI)
+
+    .then(function (response) {
+
+        return response.json();
+    })
+
+    .then(function (data) {
+
+        console.log(data);
+
+        var randomPicDiv = document.createElement("div");
+        document.body.append(randomPicDiv);
+        var randomPicImg = document.createElement("img");
+        randomPicImg.setAttribute("src", data.links.html);
+        console.log(data.links.download)
+        randomPicDiv.append(randomPicImg);
+
+    })
+
